2df79f58e012a9d6747ac4b1e44acbe258211c96
maksim_kharaneka <maksim_kharaneka@epam.com>
2015-07-14 18:21:26 +0300

    Task1

---
+++ app/components/controllers/KittyController.js
@@ -0,0 +1,47 @@
+﻿(function (module) {
+    var kittyController = function ($scope, $http) {
+
+        $scope.kitty = {
+            clickCount: 0,
+            src: '/img/cat.jpg',
+            kittyClick: function () {
+                //this.clickCount = this.clickCount + 1;
+
+                var that = this;
+                $http.get('/kittyCount').
+                    success(function (data, status, headers, config) {
+                        // this callback will be called asynchronously
+                        // when the response is available
+                        that.clickCount =  parseInt(data);
+                        that.clickCount = that.clickCount + 1;
+                        $http.get('/kittyCount?count='+that.clickCount).
+                            success(function (data, status, headers, config) {
+                                // this callback will be called asynchronously
+                                // when the response is available
+                            }).
+                            error(function (data, status, headers, config) {
+                                // called asynchronously if an error occurs
+                                // or server returns response with an error status.
+                            });
+                    }).
+                    error(function (data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                    });
+
+
+            }
+        };
+
+        //$http.get("/kittyCount")
+        //    .then(function () {
+        //        // process success
+        //        // ...
+        //    })
+        //    .catch(alerting.errorHandler("Failed to load data!"));
+
+
+    };
+    module.controller("kittyController", kittyController);
+
+}(angular.module("app")));
--- server.js
+++ server.js
@@ -10,23 +10,35 @@ app.http().io();
 app.listen(8000);
 
 // Session is automatically setup on initial request.
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     req.session.loginDate = new Date().toString();
     res.sendfile(__dirname + '/build/index.html');
 });
 app.use(expressIO.static(__dirname + '/'));
 app.use(expressIO.static(__dirname + '/build'));
 
-app.get('/mentors', function(req, res) {
+app.get('/mentors', function (req, res) {
     var result = require('./json/mentors.json');
     res.json(result);
 });
 
-app.get('/mentees', function(req, res) {
+app.get('/mentees', function (req, res) {
     var result = require('./json/mentees.json');
     res.json(result);
 });
 
+var clickCount = 0;
+//app.post('/kittyCount', function(request, response){
+//    clickCount = request.data;
+//});
+
+app.get('/kittyCount', function (request, response) {
+    var clicks = request.param('count');
+    if (typeof clicks != 'undefined'){
+        clickCount =  parseInt(clicks);
+    }
+    response.json(clickCount);
+});
 exports = module.exports = app;
 
 
